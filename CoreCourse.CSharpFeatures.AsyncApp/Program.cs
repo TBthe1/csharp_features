ec68056f7e5adb6388cdcd7ea15e832763992715
TBthe1 <bjorntruye@hotmail.com>
2020-06-01 09:14:00 +0200

    Check site status and calculate average rating

--- CoreCourse.CSharpFeatures.AsyncApp/Program.cs
+++ CoreCourse.CSharpFeatures.AsyncApp/Program.cs
@@ -1,9 +1,13 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace CoreCourse.CSharpFeatures.AsyncApp
 {
     class Program
     {
+        static SiteChecker sitechecker = new SiteChecker();
+
         static void Main(string[] args)
         {
             //collection initialization with anonymous types 
@@ -17,9 +21,33 @@ namespace CoreCourse.CSharpFeatures.AsyncApp
                 new { Url = "https://www.github.com", Rating=75 },
             };
 
+            Console.WriteLine($"Checking {siteDefinitions.Length} sites...\r\n "); 
+            
+            //check sites 
+            CheckSites(siteDefinitions.Select(e => e.Url)); 
+            
+            //calculate average 
+            var averageRating = sitechecker.AverageRating(siteDefinitions.Select(sd => sd.Rating)); 
+            Console.WriteLine($"\r\nAverage rating: {averageRating:N2}\r\n");
+
             //prevent quitting in debug mode 
             Console.WriteLine($"\r\nPress any key to exit");
             Console.Read();
+        }
+
+        static void CheckSites(IEnumerable<string> urls)
+        {
+            ConsoleColor defaultColor = Console.ForegroundColor; 
+            
+            //check sites 
+            foreach (var url in urls) 
+            {
+                var status = sitechecker.GetStatusCode(url);
+                if (status != 200) 
+                    Console.ForegroundColor = ConsoleColor.Red; 
+                Console.WriteLine($"{url} status: {status}");
+                Console.ForegroundColor = defaultColor; 
+            }
         }
     }
 }
