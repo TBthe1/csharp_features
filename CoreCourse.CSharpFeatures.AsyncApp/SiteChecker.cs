f649bcd7c8c12404682f1177841cf3b257189650
TBthe1 <bjorntruye@hotmail.com>
2020-06-01 09:08:51 +0200

    Ini CoreCourse.CSharpFeatures.AsyncApp

---
+++ CoreCourse.CSharpFeatures.AsyncApp/SiteChecker.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+
+namespace CoreCourse.CSharpFeatures.AsyncApp
+{
+    public class SiteChecker 
+    {
+        public int GetStatusCode(string siteurl) { 
+            var client = new HttpClient();
+            var httpResponse = client.GetAsync(siteurl).Result; 
+            return (int)httpResponse.StatusCode; } 
+        public double AverageRating(IEnumerable<int> ratings) {
+            return ratings.Average();
+        }
+    }
+}
